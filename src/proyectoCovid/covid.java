386d8d3a37c02d98059d73b950bf260b67e324c7
mint <mint@mint-VirtualBox>
2021-05-20 09:20:36 +0200

    primera version del proyecto,solo contiene un saludo

---
+++ src/proyectoCovid/covid.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author Adriana Martin
+ * @version 1.0.0
+ */
+
+
+
+
+package proyectoCovid;
+
+public class covid {
+	 public static void main(String[] args) {
+		 
+		 System.out.println("este es un proyecto de documentacion");
+	 }
+}
